6fce3784db09e73a64db88bea69115fe5077c4f4
naejchamp <naejchamp@tutanota.com>
2024-06-03 23:59:30 +0500

    postgres

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-# DATABASE_URI = 'mysql://root:@localhost/hru'
+
 SECRET_KEY = 'asdjasd02309jDs'
 DATABASE_URI = 'postgresql://default:ZRIEvyUqdV53@ep-mute-recipe-a4xmoa8t.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
